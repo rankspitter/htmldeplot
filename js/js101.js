b090ea656796391eb1d46bf19aed6a332f03606e
PILLENGDAO <51523749+rankspitter@users.noreply.github.com>
2023-02-18 15:37:09 +0700

    fist commit

---
+++ js/js101.js
@@ -0,0 +1,35 @@
+
+// // let x,y,z;
+// // x=5;
+// // y=6;
+
+// // z= x+y;
+// document.getElementById("demo").innerHTML= "The value of z is " + z+".";
+// let x = y + z;
+
+// Keyword	Description
+// var	Declares a variable
+// let	Declares a block variable
+// const	Declares a block constant
+// if	Marks a block of statements to be executed on a condition
+// switch	Marks a block of statements to be executed in different cases
+// for	Marks a block of statements to be executed in a loop
+// function	Declares a function
+// return	Exits a function
+// try	Implements error handling to a block of statements
+
+
+
+
+// Underscore:
+// first_name, last_name, master_card, inter_city.
+
+// Upper Camel Case (Pascal Case):
+// FirstName, LastName, MasterCard, InterCity.
+
+// Lower Camel Case:
+// firstName, lastName, masterCard, interCity.
+
+
+
+
